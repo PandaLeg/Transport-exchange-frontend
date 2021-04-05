3e250dd5ebee07b8c2840e78394d2f2b36cdb49d
Владислав Стешко <vladyslav.steshko@nure.ua>
2021-04-06 00:06:22 +0300

    Change k

--- plugins/googleMaps.js
+++ plugins/googleMaps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyD3dnyXcKkptYXseDwCI6jLrEGaOFvK0DY',
+    key: '',
     libraries: 'places'
     // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
